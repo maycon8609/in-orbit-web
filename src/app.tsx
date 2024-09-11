68dd927170cde0b3f962619429061da86c4746db
maycon silva <maycon8609@gmail.com>
2024-09-11 17:01:16 -0300

    feat: add Summary page

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,16 @@
-import { Plus } from "lucide-react";
+import { Dialog } from "./components/ui/dialog";
 
-import { Button } from "./components/ui/button";
-
-import logo from "./assets/logo-in-orbit.svg";
-import letsStart from "./assets/lets-start-illustration.svg";
+import { CreateGoal } from "./components/create-goal";
+import { Summary } from "./components/summary";
+// import { EmptyGoals } from "./components/empty-goals";
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="lets start illustration" />
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
 
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
-
-      <Button>
-        <Plus className="size-4" />
-        Cadastrar meta
-      </Button>
-    </div>
+      <CreateGoal />
+    </Dialog>
   );
 }
