f5a0d6f3b29ee50465b6b150f77bf5c438abc192
maycon silva <maycon8609@gmail.com>
2024-09-11 14:26:45 -0300

    feat: create home

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,24 @@
+import { Plus } from "lucide-react";
+
+import { Button } from "./components/ui/button";
+
+import logo from "./assets/logo-in-orbit.svg";
+import letsStart from "./assets/lets-start-illustration.svg";
+
 export function App() {
-  return <h1 className="text-xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="lets start illustration" />
+
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <Button>
+        <Plus className="size-4" />
+        Cadastrar meta
+      </Button>
+    </div>
+  );
 }
